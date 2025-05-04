6e60537dc651e822366710f6825dca31a316f3e3
Luigi <135599144+Luigi-Papalia@users.noreply.github.com>
2025-05-04 18:05:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -97,6 +97,6 @@ app.get('/calculate', (req, res) => {
     }
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Bad microservice running on port ${port}`);
 });
